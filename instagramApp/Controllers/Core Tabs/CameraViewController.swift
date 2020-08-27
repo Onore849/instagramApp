0be8b5d9f42222436fe1c6e6596361761384fb2a
Onore849 <rion3376@gmail.com>
2020-08-27 16:57:49 +0900

    finished part5

--- instagramApp/Controllers/Core Tabs/CameraViewController.swift
+++ instagramApp/Controllers/Core Tabs/CameraViewController.swift
@@ -12,19 +12,12 @@ class CameraViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        navigationController?.setNavigationBarHidden(true, animated: false)
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func didTapTakePicture() {
+    
     }
-    */
-
 }
--- instagramApp/Controllers/Core Tabs/ProfileViewController.swift
+++ instagramApp/Controllers/Core Tabs/ProfileViewController.swift
@@ -8,7 +8,9 @@
 
 import UIKit
 
-class ProfileViewController: UIViewController {
+
+/// Profile View controller
+final class ProfileViewController: UIViewController {
     
     // collectionViewの設定
     private var collectionView: UICollectionView?
@@ -19,9 +21,32 @@ class ProfileViewController: UIViewController {
         view.backgroundColor = .systemBackground
         configureNavigationBar()
         
-        collectionView = UICollectionView()
+        // ??
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        layout.sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+        layout.itemSize = CGSize(width: view.width/3, height: view.width/3)
+        collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        
+        collectionView?.delegate = self
+        collectionView?.dataSource = self
+        
+        guard let collectionview = collectionView else {
+            
+            return
+            
+        }
+        
+        view.addSubview(collectionview)
         
     }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        collectionView?.frame = view.bounds
+    }
+    
     
     private func configureNavigationBar() {
         
@@ -34,10 +59,32 @@ class ProfileViewController: UIViewController {
         let vc = SettingsViewController()
         vc.title = "Settings"
         navigationController?.pushViewController(vc, animated: true)
+    
+    }
+    
+}
+
+extension ProfileViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        return 0
         
     }
     
-
-
-
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        return UICollectionViewCell()
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+        collectionView.deselectItem(at: indexPath, animated: true)
+        
+    }
+    
+    
+    
+    
 }
